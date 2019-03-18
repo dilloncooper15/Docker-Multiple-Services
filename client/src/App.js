1398ba9490cfbf8cf32c2703f6537f66fb70a94d
Dillon Cooper <dillonjcooper2@gmail.com>
2019-03-18 12:38:51 -0400

    Testing image

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './docker_whale.png';
+import logo from './docker_whale';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
@@ -12,7 +12,9 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1><Link to="/">Home</Link> <Link to="/otherpage">Checkout Moby</Link></h1>
+            <h1 className="App-title">Fib Calculator version KUBERNETES!</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
